939423e1dee3fa92252b3b84ebe1287b88240a5c
Takhir78 <t.salimov@gmail.com>
2017-03-02 23:41:23 -0500

    working

--- buzzfizz.rb
+++ buzzfizz.rb
@@ -1,3 +1,6 @@
-def buzzfizz()
+def buzzfizz(num)
+	100.times do
+	
+	end
 
 end
--- test_buzzfizz.rb
+++ test_buzzfizz.rb
@@ -2,8 +2,8 @@ require "minitest/autorun"
 require_relative "buzzfizz.rb"
 
 class Test_buzzfizz < Minitest::Test
-	def test_1_returns_1
-		assert_equal(1, buzzfizz(1))
+	def test_returns_array_of_100_number
+		assert_equal(1..100, buzzfizz(1..100))
 	end
 
 end
